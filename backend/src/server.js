0501a66f5fef6c3fd696e27c68ad58bfccd634da
seunghoon shin <liante0904@gmail.com>
2018-07-29 22:51:35 +0900

    add dotenv, mongoose. complete mongoDB intergrate. Insert, selectAll test complete

---
+++ backend/src/server.js
@@ -0,0 +1,66 @@
+// ENV
+require('dotenv').config();
+// DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Node.js의 native Promise 사용
+mongoose.Promise = global.Promise;
+
+// CONNECT TO MONGODB SERVER
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+app.get('/api/hello', (req, res) => {
+    res.send({express: 'Hello From Express'});
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
+
+
+// Schemes
+
+const beerSchema = new mongoose.Schema({
+  beerid: { type: Number, required: true, unique: true },
+  beername: { type: String, required: true }
+},
+{
+  timestamps: true
+});
+
+
+// Create Model
+var Beer = mongoose.model('Beer', beerSchema);
+
+mongoose.model('Beer', beerSchema);
+
+// Beer 생성
+app.get('/user/insert', function(req, res) {
+  Beer.create( {
+      beerid: '1',
+      beername: '오비맥주'
+      },
+      function(err, beer) {
+          if (err) return res.status(500).send("User 생성 실패.");
+          res.status(200).send(beer);
+      });
+});
+
+
+// User 전체 조회
+app.get('/users', function(req, res) {
+  Beer.find( {}, function(err, beers) {
+      if (err) return res.status(500).send("Beer 전체 조회 실패.");
+      res.status(200).send(beers);
+  });
+});
